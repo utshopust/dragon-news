cda2af520e032cedabc0f8a0eabe385321869195
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-11-18 20:41:51 +0600

    Theme Customization, Font & Date-FNS Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3 '>
             <img className='mt-6' src={logo} alt="Logo" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            
+            <p className='text-accent font-semibold'>{format(new Date(), "EEEE, MMMM MM, yyyy ")}</p>
         </div>
     );
 };
