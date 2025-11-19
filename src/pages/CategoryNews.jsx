72e5000d05d2e905a2197f4444f311cd6b06713c
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-11-19 15:25:15 +0600

    show news to UI

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
   const { id } = useParams();
@@ -22,7 +23,16 @@ const CategoryNews = () => {
       setCategoryNews(filteredNews);
     }
   }, [data, id]);
-  return <div>Total {categoryNews.length} News Found</div>;
+  return <div>
+    <h2 className="font-bold mb-5">Total <span className="text-secondary">{categoryNews.length}</span> News Found</h2>
+    <div className="grid grid-cols-1 gap-5">
+       {
+        categoryNews.map(news => <NewsCard key={news.id}
+        news={news}
+        ></NewsCard>)
+       }
+    </div>
+  </div>;
 };
 
 export default CategoryNews;
